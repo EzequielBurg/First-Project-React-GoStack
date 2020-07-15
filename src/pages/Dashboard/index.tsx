25089e9da244330e62343373bb16f70ae8cdd2df
Ezequiel Gonçalves <ezekiel_sci@hotmail.com>
2020-07-15 00:38:32 -0300

    Updates until storage items

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,58 +1,93 @@
-import React from 'react';
+/* eslint-disable camelcase */
+import React, { useState, FormEvent, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo-img.svg';
 
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
+import api from '../../services/api';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem('@GithubExplorer');
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('@GithubExplorer', JSON.stringify(repositories));
+  }, [repositories]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+
+      setNewRepo('');
+
+      setInputError('');
+    } catch (error) {
+      setInputError('Repositório não encontrado');
+    }
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Dashboard</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repostiório" />
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite o nome do repostiório"
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/45317024?s=460&u=1687b30021ea4dd65cfbdbbea7e677fd0c90dbe3&v=4"
-            alt="user"
-          />
-          <div>
-            <strong>ezequiel/mega-sena</strong>
-            <p>App para sortear numeros para jogar na mega-sena</p>
-          </div>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/45317024?s=460&u=1687b30021ea4dd65cfbdbbea7e677fd0c90dbe3&v=4"
-            alt="user"
-          />
-          <div>
-            <strong>ezequiel/mega-sena</strong>
-            <p>App para sortear numeros para jogar na mega-sena</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/45317024?s=460&u=1687b30021ea4dd65cfbdbbea7e677fd0c90dbe3&v=4"
-            alt="user"
-          />
-          <div>
-            <strong>ezequiel/mega-sena</strong>
-            <p>App para sortear numeros para jogar na mega-sena</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
